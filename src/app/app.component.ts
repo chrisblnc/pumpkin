1a9fd4f9641dd1ebedb70bd18fe74af9acb2ea90
StiansbD <chris33et24@gmail.com>
2020-08-03 11:19:52 +0200

    firebase integration + add footer, navbar, header, and homepage

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import * as firebase from 'firebase';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,18 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'pumpkin';
+  private firebaseConfig = {
+    apiKey: "AIzaSyCneTRpvNb82URMBjeUtMzCeQJTurSVlqo",
+    authDomain: "angular-blog-7d4b0.firebaseapp.com",
+    databaseURL: "https://angular-blog-7d4b0.firebaseio.com",
+    projectId: "angular-blog-7d4b0",
+    storageBucket: "angular-blog-7d4b0.appspot.com",
+    messagingSenderId: "711545755854",
+    appId: "1:711545755854:web:b2ab10831241c11203d89f",
+    measurementId: "G-08Y2RQV071"
+  };
+
+  constructor() {
+    firebase.initializeApp(this.firebaseConfig);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { NavbarComponent } from './layout/navbar/navbar.component';
+import { FooterComponent } from './layout/footer/footer.component';
+import { HeaderComponent } from './layout/header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    NavbarComponent,
+    FooterComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
